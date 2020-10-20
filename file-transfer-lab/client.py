3ac68ac7c3add841a87fee811a66f95ee3205563
student <protogeyorocks@gmail.com>
2020-10-20 17:46:54 -0600

    fork done

--- file-transfer-lab/client.py
+++ file-transfer-lab/client.py
@@ -1,12 +1,45 @@
-import socket
-import sys
+#! /usr/bin/env python3
 
-#set up to send file
-s = socket.socket()
-s.connect(("localhost",9999))
-f = open ("happy.txt", "rb")
-l = f.read(1024)
-while (l):
-        s.send(l)
-        l = f.read(1024)
-s.close()
+# Echo client program
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
+
+from framedSock import framedSend, framedReceive
+
+switchesVarDefaults = (
+            (('-s', '--server'), 'server', "127.0.0.1:50001"),
+            (('-d', '--debug'), "debug", False), # boolean (set if present)
+            (('-?', '--usage'), "usage", False), # boolean (set if present)
+            )
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+if usage:
+            params.usage()
+try:
+        serverHost, serverPort = re.split(":", server)
+        serverPort = int(serverPort)
+except:
+        print("Can't parse server:port from '%s'" % server)
+        sys.exit(1)
+
+
+
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+s = socket.socket(addrFamily, socktype)
+
+if s is None:
+        print('could not open socket')
+        sys.exit(1)
+s.connect(addrPort)
+name = input("Receiving file")
+from framedSock import framedSend, framedReceive
+framedReceive(s)
--- file-transfer-lab/server.py
+++ file-transfer-lab/server.py
@@ -1,22 +1,43 @@
-import socket
+#! /usr/bin/env python3
+
 import sys
+sys.path.append("../lib")       # for params
 
-s = socket.socket()
-s.bind(("localhost",9999))
-s.listen(10) # listen upto 10 connections, no fork yet. this is file transfer
+import re, socket, params, os
+
+switchesVarDefaults = (
+        (('-l', '--listenPort') ,'listenPort', 50001),
+        (('-d', '--debug'), "debug", False), # boolean (set if present)
+        (('-?', '--usage'), "usage", False), # boolean (set if present)
+        )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+print("listening on:", bindAddr)
 while True:
-    sc, address = s.accept()
-    i=1
-    f = open('file_'+ str(i)+".txt",'wb') #open
-    i=i+1
+    sock, addr = lsock.accept()
+    from framedSock import framedSend, framedReceive
 
-
-    while (True):
-        # receive data and write it to file
-        l = sc.recv(1024)
-        while (l):
-            f.write(l)
-            l = sc.recv(1024)
-    f.close()
-    sc.close()
-s.close()
+    name = input("What is the file name, include its extension")
+    if not os.fork():
+        print("new child process handling connection from", addr)
+        while True:
+            framedSend(sock, name)
+            #payload = framedReceive(sock, name)
+            #if debug: print("rec'd: ", payload)
+            #if not payload:
+                #if debug: print("child exiting")
+                #sys.exit(0)
+            #payload += b"!"
+            #framedSend(sock)
+            
